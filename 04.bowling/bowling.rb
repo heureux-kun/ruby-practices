a1a634c85bcfc5099842fbb4fb69329c49024c26
heureux-kun <uenishi@umlautone.jp>
2022-03-20 22:29:26 +0900

    完成

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -22,30 +22,10 @@ shots.each_slice(2) do |s|
 end
 
 point = 0
-count = 0
+current_status = ''
 prev_status = ''
-prev_prev_status = ''
-frames.each do |frame|
-  count += 1
-  point +=
-    case prev_status
-    when 'spare'
-      frame[0] + frame.sum
-    when 'strike'
-      frame.sum * 2
-    else
-      frame.sum
-    end
-
-  # 前々回と前回がストライクだった時、さらにframe[0]を加算する
-  if prev_prev_status == 'strike' && prev_status == 'strike' && count != 11
-    point += frame[0]
-  end
-
-  # binding.irb
-
-  prev_prev_status = prev_status
-  prev_status =
+frames.each_with_index do |frame, index|
+  current_status = 
     if frame[0] == 10
       'strike'
     elsif frame.sum == 10
@@ -53,5 +33,32 @@ frames.each do |frame|
     else
       ''
     end
+
+  if index < 9
+    point +=
+      case current_status
+      when 'spare'
+        # 次の1投の点を加算する
+        frame.sum + frames[index+1][0]
+        # binding.irb
+      when 'strike'
+        # 次の2投の点を加算する
+        # 次がスペアだった場合
+        if frames[index+1][0] != 10
+          frame.sum + frames[index+1].sum
+        # 次がストライクだった場合
+        else
+          frame.sum + frames[index+1][0] + frames[index+2][0]
+        end
+      else
+        frame.sum
+      end
+  else
+    point += frame.sum
+  end
+
+  prev_status = current_status
+  # puts "#{index+1}回目"
+  # binding.irb
 end
 puts point
